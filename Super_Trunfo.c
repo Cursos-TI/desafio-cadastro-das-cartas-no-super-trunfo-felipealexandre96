54859c4a0d6d0a712ae388201516510d5cbcdc33
felipealexandre96 <felipealexandrebarbosa@gmail.com>
2025-03-07 21:18:25 +0000

    mais uns passos

--- Super_Trunfo.c
+++ Super_Trunfo.c
@@ -1,28 +1,22 @@
 #include<stdio.h>
 
 int main(){
-    char estado1;
-    char codigo1[50];
+    char estado1 = 'a';
+    char codigo1[50] = 'a01';
     char nome1[50];
     int populacao1;
     float area1;
     float PIB1;
     int pontos1;
 
-    printf("digite o estado da cidade 1: \n");
-    scanf("%c", &estado1);
-
-    printf("digite o codigo da cidade 1: \n");
-    scanf("%S", codigo1);
-
     printf("digite o nome da cidade 1: \n");
-    scanf("%S", nome1);
+    scanf("%s", nome1);
 
     printf("digite a populacao da cidade 1: \n");
     scanf("%d", &populacao1);
 
     printf("digite a area da cidade 1: \n");
-    scanf("%f", area1);
+    scanf("%f", &area1);
 
     printf("digite o PIB da cidade 1: \n");
     scanf("%f", &PIB1);
@@ -30,10 +24,14 @@ int main(){
     printf("digite os pontos turisticos da cidade 1: \n");
     scanf("%d", &pontos1);
 
-    return 0;
-
-
-
+    printf("carta 1: \n");
+    printf("Estado: %c", estado1);
+    printf("Codigo: %s", codigo1);
+    printf("Nome da cidade: %s", nome1);
+    printf("População: %d", populacao1);
+    printf("Área: %f", area1);
+    printf("PIB: %f Bilhões de reais", PIB1);
+    printf("Número de pontos turisticos: %d", pontos1);
 
 
 }
